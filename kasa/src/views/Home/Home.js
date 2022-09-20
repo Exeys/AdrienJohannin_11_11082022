5575f97f6bd71013c007c2a357925129de62e53e
Adrien <47904625+Exeys@users.noreply.github.com>
2022-09-20 16:36:01 +0200

    fix banner brightness

--- kasa/src/views/Home/Home.js
+++ kasa/src/views/Home/Home.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Thumb from '../../components/Thumb/Thumb.js';
 import './Home.css'
 import data from '../../data/logements.json'
+import banner from '../../assets/banner.png'
 
 const Home = () => {
     return (
         <div className='HomeContent'>
             <div className='HomeBanner'>
+                <img className='HomeBannerImage' src={banner} alt="banner" />
+
                 <div className='HomeBannerText'>
                     <p>Chez vous, partout et ailleurs</p>
-                    </div>
+                </div>
             </div>
             <div className='Thumbs'>
                 {data.map((apartment, i) => {
